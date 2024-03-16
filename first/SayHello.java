8a535fde6f9cab73831ca8404381255af8d66dc1
Timorleiderman <Timorleiderman@gmail.com>
2024-03-16 14:12:45 +0200

    first

---
+++ first/SayHello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SayHello
+{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        int x;
+        double d;
+        x = scan.nextInt();
+        d = scan.nextDouble();
+        // System.out.println(x);
+    }
+    
+}
