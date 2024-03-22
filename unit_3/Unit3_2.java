8142a7283c4b784fc0e7d654f3f3bd9e23c9cdcc
Timor <Timorleiderman@gmail.com>
2024-03-22 10:10:18 +0200

    Create Unit3_2.java

---
+++ unit_3/Unit3_2.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Unit3_2
+{
+    	public static void main(String[] args)
+    	{
+       		Date openingDate = new Date (125, 3, 15);
+        	Date endingingDate = new Date (125, 3, 25);
+        	System.out.print(openingDate  + "-");
+        	System.out.println(endingingDate);
+
+    	}
+}
