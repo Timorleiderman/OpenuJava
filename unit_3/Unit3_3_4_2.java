f1161b9072daf3f63b415e5e046fa96d79ab0290
Timor <Timorleiderman@gmail.com>
2024-03-22 10:10:27 +0200

    Create Unit3_3_4_2.java

---
+++ unit_3/Unit3_3_4_2.java
@@ -0,0 +1,33 @@
+
+import java.util.Random;
+
+public class Unit3_3_4_2 {
+    public static void main(String [] args) {
+
+        Random rand = new Random();
+
+        String s = "Hello World";
+        System.out.println(s.charAt(s.length()-1));     
+        System.out.println(s.equals("Hello world"));     
+
+        int num = rand.nextInt(13) + 1;
+        int type = rand.nextInt(4);
+        String type_name = "";
+
+        switch (type) {
+            case 0:
+                type_name = "Heart";
+                break;
+            case 1:
+                type_name = "Club";
+                break;
+            case 2:
+                type_name = "Diamond";
+                break;
+            case 3:
+                type_name = "Spade";
+        }
+        System.out.println("card = " + num + " type = " + type_name);
+
+    }
+}
