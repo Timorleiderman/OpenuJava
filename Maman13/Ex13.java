1fdd46e6da0aabba1440d4ff59698ccbcc8201dd
Timorleiderman <Timorleiderman@gmail.com>
2024-05-26 00:51:16 +0300

    Update Ex13.java

--- Maman13/Ex13.java
+++ Maman13/Ex13.java
@@ -2,7 +2,8 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 public class Ex13 {
     
-    private static final int ALPHABET_LENGTH = 24;
+    private static final int ALPHABET_LENGTH = 26;
+    private static final int RECRUSION_DEPTH = 4000;
     /**
      * ********** Question 1 **********
      *  like binery search compare even index if the even index and nex are the same the number is on the right
@@ -88,30 +89,74 @@ public class Ex13 {
         return water;
     }
 
-    
     public static int cntTrueReg (boolean [][] a){
         return 1;
     }
 
+    /**
+     * it will call the private method to recrusivvly find the password
+     * @param p - Password .class object with isPassword method to check if the guess is correct
+     * @param length - length of the password
+     * @return password String found
+     */
     public static String findPassword(Password p, int length){
-        return findPassword(p, length, generatePassword(length, 0), 0);
+        return findPassword(p, length, 0, -1);
     }
 
-    private static String findPassword(Password p, int length, String guess, int idx){
+    /**
+     * This method will call recrusivly the search index method with a counter parameter to count the number of calls
+     * the counter is to prevent stck overflow for large strings
+     * this code can only work with string up to 5 charecters 
+     * for more charecters you will need to increese the stack size
+     * @param p - Password .class object with isPassword method to check if the guess is correct
+     * @param length - length of the password
+     * @param idx - string index represent the needed string to be generated
+     * @param resIdx - index of the found Password string
+     * @return password String found
+     */
+    private static String findPassword(Password p, int length, int idx, int resIdx) {
+        if (resIdx != -1)
+            return generatePassword(length, resIdx);
 
+        int newResIdx = findPassword(p, length, generatePassword(length, idx), idx, 0);
+        return findPassword(p, length, idx + RECRUSION_DEPTH + 1, newResIdx);
+    }
+
+    /**
+     * 
+     * @param p - Password .class object with isPassword method to check if the guess is correct
+     * @param length - length of the password
+     * @param guess - password guess string
+     * @param idx - index that represents the guess
+     * @param cnt - counter to protect agains stuck overflow
+     * @return index of the correct string
+     */
+    private static int findPassword(Password p, int length, String guess, int idx, int cnt) {
+        // System.out.println("GUESS: " + guess + "  needs + " + p.getPassword());
+        
         if (p.isPassword(guess)) 
-            return guess;
+            return idx;
 
-        return findPassword(p, length, generatePassword(length, idx+1), idx+1);
+        if (cnt >= RECRUSION_DEPTH)
+            return -1;
+        
+        String nextGuess = generatePassword(length, idx + 1);
+        return findPassword(p, length, nextGuess, idx+1, cnt+1);
+
     }
 
+    /**
+     * recrusive method to create string with the length given
+     * for very large string it will have stuck overfow
+     * @param length - length of the password
+     * @param idx - index that represents the guess
+     * @return string that represents the index
+     */
     private static String generatePassword(int length, int idx) {
-
-        if (length<= 0)
+        if (length <= 0)
             return "";
-
         char suffix = (char) ('a' + idx % ALPHABET_LENGTH);
-        return generatePassword(length - 1, idx / ALPHABET_LENGTH) + suffix;
+        return suffix + generatePassword(length - 1, idx / ALPHABET_LENGTH);
     }
 
 }   
