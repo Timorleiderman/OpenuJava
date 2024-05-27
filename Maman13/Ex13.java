115ab1311fe86a91230868a38aa55f6b2e214c01
Timorleiderman <Timorleiderman@gmail.com>
2024-05-27 19:42:44 +0300

    Update Ex13.java

--- Maman13/Ex13.java
+++ Maman13/Ex13.java
@@ -33,32 +33,13 @@ public class Ex13 {
         
         return a[left];
     }
-    /**
-     * ********** Question 1 **********
-     *  iterate over all elements worst case it will iterate all the elemtns
-     *  int the best case it will iterate over the first two
-     *  Time complexity O(n)
-     *  Space complexity O(1)
-     * @param a non empty input array with one integer that not appear twice in a row
-     * @return the single int that was found
-     */
-    // public static int findSingle(int [] a) {
-       
-    //     for (int i = 0; i < a.length - 1; i += 2) {
-    //         if (a[i] != a[i + 1]) {
-    //             return a[i];
-    //         }
-    //     }
-    //     return a[a.length - 1];
-    // }
-
 
     /**
      * ********** Question 2 **********
      * this function will iterate for each tower finding the max tower to the left and to the right
      * then i will add to the total water sum the minimal tower found less the the hight of the current tower
-     *  Time complexity O(n*n) = O(n^2)
-     *  Space complexity O(1)
+     * Time complexity O(n*n) = O(n^2)
+     * Space complexity O(1)
      * @param a array of towers hights positive integers non zero
      * @return water volume to fill
      */
@@ -68,6 +49,7 @@ public class Ex13 {
         int rightMax;
         int water = 0;
         int minLeftRigth;
+
         for (int i = 1; i < a.length - 1; i++){
             leftMax = 0;
             rightMax = 0;
@@ -79,10 +61,11 @@ public class Ex13 {
             for (int r = i+1; r < a.length; r++) {
                 rightMax = Math.max(rightMax, a[r]);
             }
-
+            // the smallest from the max from left or right will be the water level
             minLeftRigth = Math.min(leftMax, rightMax);
             
             if (minLeftRigth > a[i]) {
+                // add the water level of the min max and subract current cell level
                 water = water + (minLeftRigth - a[i]);
             }
         }
@@ -92,18 +75,20 @@ public class Ex13 {
 
     /**
      * publlic method that will call the recrusive private method to count all the regions in the matrix
+     * The recrusive method will go over all the matrix cells and will count and clear each region 
+     * this method will change the value of each place in the matrix to false and will count when entering to a new region
+     * 
+     * Time complexity O(rows*columns) = O(n*n) = O(n^2)
+     * Space complexity O(1)
+     * 
      * @param a boolean matrix represents the regions
      * @return number of regions
      */
-    public static int cntTrueReg (boolean [][] a){
+    public static int cntTrueReg(boolean [][] a){
         return cntTrueReg(a, 0, 0);
     }
 
     /**
-     * recrusive method that will go over all the matrix cells and will mark each region
-     * this method will change the value of each place in the matrix to false and will count when entering to a new region
-     * Time complexity O(rows*columns) = O(n*n) = O(n^2)
-     * Space complexity O(1)
      * @param a boolean matrix represents the regions
      * @param row row index
      * @param col column index
@@ -158,7 +143,12 @@ public class Ex13 {
         }
     }
     /**
-     * it will call the private method to recrusivvly find the password
+     * it will call the private method to recrusivly find the password
+     * The recrusive private method will try all english alphabet strings with the given length 
+     * it will count the number of calls for the recrusion depth to prevent stack overflow
+     * this code can only work with string up to 5 charecters 
+     * for 6 charecter string you will need to run it with -Xss16m
+     * for more charecters you will need to increese the stack size even more
      * 
      * @param p - Password .class object with isPassword method to check if the guess is correct
      * @param length - length of the password
@@ -169,12 +159,8 @@ public class Ex13 {
     }
 
     /**
-     * This method will call recrusivly the search index method with a counter parameter to count the number of calls
-     * the counter is to prevent stck overflow for large strings
-     * this code can only work with string up to 5 charecters 
-     * for 6 charecter string you will need to run it with -Xss16m
-     * for more charecters you will need to increese the stack size
-     * 
+     * This method will call recrusivly and try all posible strings with the given length
+     * here may be a stack overflow for large strings
      * Time complexity O(n) 
      * Space complexity O(1)
      * 
@@ -194,6 +180,8 @@ public class Ex13 {
     }
 
     /**
+     * This method will call recrusivly and try all posible strings with the given length
+     * the depth is to protect from stack overflow
      * 
      * @param p - Password .class object with isPassword method to check if the guess is correct
      * @param length - length of the password
@@ -203,7 +191,6 @@ public class Ex13 {
      * @return index of the correct string
      */
     private static String findPassword(Password p, int length, int idx, int depth) {
-        
         
         if (depth > RECRUSION_DEPTH)
             return "0";
@@ -219,8 +206,6 @@ public class Ex13 {
     /**
      * recrusive method to create string with the length given
      * for very large string it will have stuck overfow
-     * Time complexity O(n) 
-     * Space complexity O(1)
      * 
      * @param length - length of the password
      * @param idx - index that represents the guess
