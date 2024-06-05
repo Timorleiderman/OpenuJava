98d11f7a2ca7a99a1b30b3a074ae3db317dc3382
Timor <Timorleiderman@gmail.com>
2024-06-05 22:13:36 +0300

    Update IntListTwo.java

--- Maman14/IntListTwo.java
+++ Maman14/IntListTwo.java
@@ -1,10 +1,17 @@
+/**
+ * Maman14 Exersize 
+ * 
+ * @author (Timor Leiderman) 
+ * @version (2024)
+ */
+
 public class IntListTwo
 {
     IntNodeTwo _head, _tail;
-    // ùéîå ìá ! áëååğä, åáğéâåã ìîä ùìîãúí á÷åøñ,
-    // ìà äâãøğå àú ùúé äúëåğåú äììå ë-private.
-    // äñéáä ìëê äéà ìöåøê äáãé÷ä ùìğå àú äòáåãä ùìëí.
-    // äùàéøå äâãøä æàú ëôé ùäéà åàì úùğå àåúä !
+    // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ! ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½, ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½,
+    // ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½-private.
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½.
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ !
 
     public IntListTwo()
     {
@@ -68,12 +75,62 @@ public class IntListTwo
         return result;
     }
 
-    public int win ()
+    /**
+     * 
+     * the strategy is to calculate the sum of the even and the sum of the odd and take the bigger one
+     * if we mark all even nodes in black and all odd ones in white 
+     * if the first player starts from the right (took the odd coin) the second player can take only the second and the 2*n coin
+     * so the strategy will be to take all the even or all the odds
+     * for wining the first needs to take the bigger sum
+     * 
+     * Time complexity O(n)
+     * Space complexity O(1)
+     * 
+     * 
+     * @return the difference betwen the odd sum and the even sum
+     */
+    public int win()
     {
-        // write your code here
-        return 0;
+        return Math.abs(sumSkip(_head) - sumSkip(_head.getNext()));
     }
 
+    /**
+     * 
+     * this method will calculate the sum of all nodes but will skip one each time
+     * 
+     * Time complexity O(n)
+     * Space complexity O(1)
+     * 
+     * @param node the node to start from
+     * @return the sum of all nodes when skips one each time
+     */
+    private int sumSkip(IntNodeTwo node) {
+
+        if (node == null)
+            return 0;
+
+        int sum = node.getNum();
+        IntNodeTwo temp = node;
+        while (temp != null && temp.getNext() != null) {
+            sum += temp.getNum();
+            temp = temp.getNext().getNext();
+        }
+
+        return sum;
+    }
+
+    /**
+     * Calculate the sum from low node to high
+     * 
+     * Time complexity O(n)
+     * Space complexity O(1)
+     * 
+     * @param low index of the node from where to start summing
+     * @param high index fo the node to stop summing
+     * @return the sum from node low to high
+     * 
+
+     */
     private int f (int low, int high)
     {
         int result = 0;
@@ -88,6 +145,19 @@ public class IntListTwo
         return result;
     }
 
+    /**
+     * finds the sum of a sub linked list that will sum to num
+     * if it finds the sum equal to num it will print to stdout
+     * between ## and ##
+     * otherwise it will print 
+     * No 
+     * n + (n-1) + (n-2) .... 1 = n * ( n + 1 ) / 2
+     * Time complexity O(n*n) = O(n^2)
+     * Space complexity O(1)
+     * 
+     * @param num the sum of the sub linked list to search
+     * @return true if found a sub linked list with sum equal to num
+     */
     public boolean what (int num)
     {        
         int listSize = size();
@@ -108,8 +178,33 @@ public class IntListTwo
 
     public boolean betterWhat (int num)
     {
-        // write your code here
-        return true;
+        IntNodeTwo start = _head;
+        IntNodeTwo end = _head;
+        int currentSum = 0;
+        int i = 0;
+        int j = 0;
+        
+        while (end != null) {
+            currentSum += end.getNum();
+
+            while (currentSum > num && start != end) {
+                currentSum -= start.getNum();
+                start = start.getNext();
+                i++;
+            }
+
+            if (currentSum == num) {
+                System.out.println("between " + i + " and " + j);
+                return true;
+            }
+            
+            end = end.getNext();
+            j++;
+                
+        }
+
+        System.out.println("No ");
+        return false;
     }
     
     public int longestCommonSublist(IntListTwo list2)
